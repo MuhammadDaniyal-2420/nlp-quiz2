7690402d98940f8f30057eee33038a1c488f29ff
MuhammadDaniyal-2420 <126562760+MuhammadDaniyal-2420@users.noreply.github.com>
2023-11-24 23:32:56 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load pre-trained text classification model from Hugging Face
+classifier = pipeline("sentiment-analysis")
+
+# Streamlit app
+def main():
+    st.title("Text Classification App")
+    st.write("Enter text below to get sentiment analysis predictions.")
+
+    # User input for text
+    user_input = st.text_area("Enter text:")
+
+    # Make prediction when the user clicks the button
+    if st.button("Get Sentiment Analysis"):
+        if user_input:
+            # Perform text classification
+            result = classifier(user_input)
+            st.write("Prediction:", result[0]['label'])
+            st.write("Score:", result[0]['score'])
+        else:
+            st.warning("Please enter text for analysis.")
+
+# Run the app
+if __name__ == "__main__":
+    main()
